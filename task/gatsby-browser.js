bd29180afe0f5a37780e3d95fc6522b626e3d8fd
slobodankasvirkova <58328723+slobodankasvirkova@users.noreply.github.com>
2020-08-24 17:58:50 +0200

    design

--- task/gatsby-browser.js
+++ task/gatsby-browser.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import "./src/styles/global.css"
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/css/bootstrap.css'
--- task/src/pages/index.js
+++ task/src/pages/index.js
@@ -1,4 +1,65 @@
 import React from "react"
 export default function Home() {
-  return <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+  return (
+    <div>
+        <h5>Plans&Prices</h5>
+        <div className="container">
+        <div class="column">
+          <ul class="price">
+            <li class="header">FREE</li>
+            <li class="grey">$ 0/mo</li>
+            <li><div className="arrow">&#8594;</div>   1 user</li>
+            <li><div className="arrow">&#8594;</div>   50 MB storage</li>
+            <li><div className="arrow">&#8594;</div>   1 address</li>
+            <li><div className="arrow">&#8594;</div>   No domain support</li>
+            <li class="free"><div className="arrow">&#8594;</div>   ProtonVPN (Optional)*</li>
+            <li class="grey"><a href="#" class="button">Sign Up</a></li>
+          </ul>
+        </div>
+        
+        <div class="column plus">
+          <ul class="price">
+            <li class="header">PLUS</li>
+            <li class="grey">$ 4/mo</li>
+            <li><div className="arrow">&#8594;</div>   1 user</li>
+            <li><div className="arrow">&#8594;</div>   5 GB storage*</li>
+            <li><div className="arrow">&#8594;</div>   5 address*</li>
+            <li><div className="arrow">&#8594;</div>   Supports 1 domain*</li>
+            <li><div className="arrow">&#8594;</div>   Supports folders,labels,filters,auto-reply,IMTP/SMPT and more</li>
+            <li><div className="arrow">&#8594;</div>   ProtonVPN (Optional)*</li>
+            <li class="grey"><a href="#" class="button">Sign Up</a></li>
+          </ul>
+        </div>
+        
+        <div class="column professional">
+          <ul class="price">
+            <li class="header">PROFESSIONAL</li>
+            <li class="grey">$ 6.25/mo</li>
+            <li><div className="arrow">&#8594;</div>   1 - 5000 user*</li>
+            <li><div className="arrow">&#8594;</div>   5 GB storage per user*</li>
+            <li><div className="arrow">&#8594;</div>   5 addresses per user*</li>
+            <li><div className="arrow">&#8594;</div>   Supports 2 domains*</li>
+            <li><div className="arrow">&#8594;</div>   Catch all email,multi user menagement,priority support and more</li>
+            <li><div className="arrow">&#8594;</div>   ProtonVPN (Optional)*</li>
+            <li class="grey"><a href="#" class="button">Sign Up</a></li>
+          </ul>
+          </div>
+          <div class="column visionary">
+          <ul class="price">
+            <li class="header">VISIONARY</li>
+            <li class="grey">$ 24/mo</li>
+            <li><div className="arrow">&#8594;</div>   6 user</li>
+            <li><div className="arrow">&#8594;</div>   20 GB storage</li>
+            <li><div className="arrow">&#8594;</div>   50 addresses</li>
+            <li><div className="arrow">&#8594;</div>   Supports 10 domains</li>
+            <li><div className="arrow">&#8594;</div>   Includes all features</li>
+            <li><div className="arrow">&#8594;</div>   Priority support</li>
+            <li class="visionary"><div className="arrow">&#8594;</div>   Includes ProtonVPN</li>
+            <li class="grey"><a href="#" className="button">Sign Up</a></li>
+          </ul>
+          </div>
+          </div>
+          </div>
+
+  )
 }
